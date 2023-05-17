e930f091685ca746b1029a0d41cb9fc5b9f423cf
tomaskul <16058999+tomaskul@users.noreply.github.com>
2023-05-17 13:22:21 +0100

    Add VenueEntity

---
+++ library/src/main/java/io/github/tomaskul/dddroomwithviewdomain/RoomReservation/VenueEntity.java
@@ -0,0 +1,17 @@
+package io.github.tomaskul.dddroomwithviewdomain.RoomReservation;
+
+import io.github.tomaskul.dddroomwithviewdomain.DomainEntity;
+
+import java.util.List;
+
+public class VenueEntity extends DomainEntity<Long> {
+
+    public String Name;
+    public List<RoomEntity> AvailableRooms;
+
+    public VenueEntity(long id, String name, List<RoomEntity> availableRooms) {
+        this.Id = id;
+        this.Name = name;
+        this.AvailableRooms = availableRooms;
+    }
+}
