75886c1c7232b7c6bf05741ac8032fb186e3c8ec
tomaskul <16058999+tomaskul@users.noreply.github.com>
2023-05-17 11:03:21 +0100

    Delete MyService and example usage

--- application/src/main/java/io/github/tomaskul/dddroomwithviewapplication/RoomWithViewApplication.java
+++ application/src/main/java/io/github/tomaskul/dddroomwithviewapplication/RoomWithViewApplication.java
@@ -1,15 +1,11 @@
 package io.github.tomaskul.dddroomwithviewapplication;
 
-import io.github.tomaskul.dddroomwithviewdomain.MyService;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
 public class RoomWithViewApplication {
-
-    private static MyService service;
     public static void main(String[] args) {
         SpringApplication.run(RoomWithViewApplication.class, args);
-        service.helloWorld();
     }
 }
--- library/src/main/java/io/github/tomaskul/dddroomwithviewdomain/MyService.java
+++
@@ -1,5 +0,0 @@
-package io.github.tomaskul.dddroomwithviewdomain;
-
-public class MyService {
-    public static String helloWorld(){ return "hello world from domain library!";}
-}
